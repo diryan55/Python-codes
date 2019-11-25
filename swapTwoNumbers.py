a0f971dd49c168648e6f1a7ea731ac5731b998db
diryan55 <57243767+diryan55@users.noreply.github.com>
2019-11-24 20:08:36 -0800

    1st version to add two numbers

---
+++ swapTwoNumbers.py
@@ -0,0 +1,20 @@
+#this program take input from user and swap the value.
+
+#taking inputs from the user
+print("please input to swap")
+first_number = input()
+print("please input to swap")
+second_number = input()
+
+#swapping the value taking temp varriable  and printing the value.
+third_number = first_number
+print("third number is " , third_number)
+first_number =  second_number
+print ("first number is", first_number)
+second_number = third_number
+print("second number is" ,second_number)
+
+
+
+
+
